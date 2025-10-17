2ab9ff46a7da509acadf9f2f948633d3037108fe
kosuke <kosuke.m.dev@gmail.com>
2025-10-17 17:03:05 +0900

    再コミット

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-// In serverless / hot-reloading environments creating multiple instances of
-// PrismaClient can lead to exhaustion of database connections or errors.
-// Use a global variable to reuse the client during development.
-declare global {
-	// allow connecting to globalThis in Node.js for a cached PrismaClient
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	// eslint-disable-next-line no-var
-	var __prisma: PrismaClient | undefined;
-}
-
-const prisma: PrismaClient = (globalThis as any).__prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") (globalThis as any).__prisma = prisma;
+const prisma = new PrismaClient();
 
 export default prisma;
