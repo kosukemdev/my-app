fc3c7603c89ab709745bb58a452e7f7d1bfebb9f
kosuke <kosuke.m.dev@gmail.com>
2025-10-22 12:38:00 +0900

    env

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,7 @@ const nextConfig: NextConfig = {
   eslint: {
     // デプロイ時のLintエラーを無視
     ignoreDuringBuilds: true,
-  },
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
-  },
-  // PrismaをEdgeではなくNodeで動かす
-  output: "standalone",
+  }
 };
 
 export default nextConfig;
