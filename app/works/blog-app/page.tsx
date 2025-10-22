434c510e095ccd1f3383ff03f5448b497091e72e
kosuke <kosuke.m.dev@gmail.com>
2025-10-22 15:19:47 +0900

    DB接続修正

--- app/works/blog-app/page.tsx
+++ app/works/blog-app/page.tsx
@@ -6,6 +6,8 @@ import { getServerSession } from "next-auth";
 import authOptions from "@/lib/auth";
 import prisma from "@/lib/prisma";
 
+export const dynamic = 'force-dynamic';
+
 export const metadata = {
   title: "Blog App | Kosuke Masaki Portfolio",
   description: "シンプルなブログアプリケーションです。",
