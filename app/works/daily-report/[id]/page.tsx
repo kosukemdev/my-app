cd2d6c18e6cdc2a12001a79f6673e6ebcc99c984
kosukemdev <kosuke.m.dev@gmail.com>
2026-03-08 09:41:02 +0900

    daily-report DetailPage関数のparams型をPromise<{ id: string }>に修正し、awaitを追加して非同期処理を適切に処理 @types/reactと@types/react-domのバージョンを更新

--- app/works/daily-report/[id]/page.tsx
+++ app/works/daily-report/[id]/page.tsx
@@ -16,9 +16,9 @@ const isSameDate = (date1: string, date2: string): boolean => {
 export default async function DetailPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const { id } = params;
+  const { id } = await params;
 
   const { data, error } = await supabase
     .from("posts")
